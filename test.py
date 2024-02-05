a6a8597ea29137eff88fb7d4d72afb7f0d41d037
itsrocchi <93975501+itsrocchi@users.noreply.github.com>
2024-02-05 16:25:32 +0100

    modfiy test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import json
 import paho.mqtt.client as mqtt
+import random
 # Define MQTT broker details
 broker_address = "localhost"  # Replace with your broker's IP address or hostname
 broker_port = 1883
@@ -11,14 +12,28 @@ client = mqtt.Client()
 client.connect(broker_address, broker_port)
 
 # Create the message payload as a JSON
-payload = {
-    "x": 31,
-    "y": 35,
-    "z": 36
-}
+#
+#    "x": 31,
+#    "y": 35,
+#    "z": 36
+#}
+
+
+def generate_data():
+    latitudine = random.uniform(-90, 90)  # Intervallo per latitudine
+    longitudine = random.uniform(-180, 180)  # Intervallo per longitudine
+    magnitudo = random.uniform(1, 10)  # Intervallo per magnitudo
+
+# Create the message payload as a JSON
+    payload = {
+        "latitudine": latitudine,
+        "longitudine": longitudine,
+        "magnitudo": magnitudo
+    }
+    return json.dumps(payload)
 
 # Convert the payload to JSON string
-message = json.dumps(payload)
+message = generate_data()
 
 # Publish the message to the topic
 topic = "/zone1/accelerometer"
