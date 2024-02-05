56ecac2f327235c4d94aba6f76a684fd1abe523c
BeatriceTomassi <beatrice.tomassi@student.univaq.it>
2024-02-05 17:54:32 +0100

    generazione dati sensori

---
+++ geofonoX.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_normal_data_geoX():
+    x = random.uniform(0.1 , 0.75)
+    return x
+
+# Funzione per generare dati allarmistici
+def generate_alarm_data_geoX():
+    x = random.uniform(0.75, 1)
+    return x
